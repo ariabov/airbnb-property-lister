0dab701509d731587ee056ee67ee6cc598462f5e
ariabov <spralex@gmail.com>
2013-06-15 13:22:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirbnbPropertyLister::Application.config.secret_token = '71668f5880e542616995954b4070084252d2390fac4ac414b6654ccc4dc83c6228027ea569c565ce6cd13b91b4ddf85efaa83c83f0c07452c02761d2fb64cb95'
